71ba5edd7bbdee1d8f1e4e6586d3356c0aea420c
김다슬 <daseul727@gmail.com>
2025-03-24 13:40:50 +0900

    Noto sans KR 폰트 추가

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: Image.asset('assets/character.png')
+      theme: ThemeData(
+        fontFamily: 'NotoSansKR'
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Image.asset('assets/character.png'), onPressed: () {
+              //아이콘 클릭 시 동작
+            },
+          ),
+          title: SizedBox(
+            height: 40,
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: '검색어를 입력하세요', //힌트 텍스트
+                filled: false, //배경색 활성화
+                fillColor: Colors.white,
+                suffixIcon: Icon(Icons.search, color: Colors.grey),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: Colors.grey, width: 1)
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.grey, width: 1)
+                ),
+                contentPadding: EdgeInsets.only(left: 15, right: 10)
+              ),
+            ),
+          ),
+        ),
+        body: Container(
+          width: 150, height: 50, color: Colors.black,
+          margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        ),
+        bottomNavigationBar: BottomAppBar(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(Icons.star),
+                  Icon(Icons.confirmation_num_sharp),
+                  Icon(Icons.co2_outlined)]
+            )
+        )
+      )
     );
 
   }
